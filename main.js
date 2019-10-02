9ca90f4752e7598f8da70f29acb6d53f7057ff42
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-02 14:48:17 -0400

    hello there complete

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-function isHot() {
+function isHot(temp) {
+  return (temp>=80) ? 'Yes, it is indeed hot.' : 'No, it is not hot.'
+  }
 
-}
 
-
-function helloThere() {
-
+function helloThere(str) {
+  return (str.length<=5) ? 'Hello' + ', ' + str : 'Hi' + ', ' + str ;
 }
 
 
